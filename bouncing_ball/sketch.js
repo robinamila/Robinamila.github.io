2f92179adde1d04a0885d904804e816ff74ed566
robinamila <robinamila66@gmail.com>
2020-12-03 09:32:13 +0100

    h

--- bouncing_ball/sketch.js
+++ bouncing_ball/sketch.js
@@ -1,24 +1,25 @@
-var x = 0; //Variable der gør at x er = 0
-var speed = 10;
+let x = 320;
+let y = 180;
+let xspeed = 10;
+let yspeed = 15;
+
+let r = 25;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight); //Laver et canvas der passer til "vinduet"
-  }
- 
-function draw() {
-  background(220);
-  ellipse (x,windowHeight/2,100,100);//Laver en ellipse der starter halvejs ned af y-aksen ved hjælå af "windowHeight/2"
+  createCanvas(windowWidth, windowHeight);
   
-  x=x+speed; //Gør at x bliver plusset med 10 ved hjælp af vores anden variable "var speed =10"
   
-  if (x>windowWidth){
-    speed=-10 //Denne kode gør at hver gang vores cirkel rammer enden(højre side) af vinduet "windowWidth" bliver den sendt tilbage ved x værdien -10
-  }
-  
-  else if (x<=0){
-    speed=10 // Denne kode gør at hvis cirklens x-værdi er lig med eller mindre end 0 bliver den sendt til højre side igen med værdien +10
-  }
+}
 
-  
-  
+function draw() {
+  background("pink")
+  ellipse(x, y, r*2, r*2)
+  x += xspeed
+  y += yspeed
+  if (x > windowWidth - r || x < r) {
+    xspeed = -xspeed
+  }
+  if (y > windowHeight - r || y < r ) {
+    yspeed = -yspeed
+  } 
 }
